8a11bff25a0ad7b6e7b636de928a7536795fdc0b
Gokul0201 <kgokula314@gmail.com>
2022-12-25 21:54:56 +0530

    commit

--- src/Components/EditProfile.js
+++ src/Components/EditProfile.js
@@ -118,7 +118,7 @@ const Profile = () => {
         </Box>
       <Box
         sx={{
-          marginTop: 8,
+          marginTop: 3,
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
